7517357ae44e08531ecbf25fbb85ba4ff48d5c86
david <apazareynaldo212390@gmail.com>
2024-10-14 16:19:01 +0200

    rate limit

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
 const gameRoutes = require('./routes/game');
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+const loginLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 15 minutos
+  max: 3, // limite de 5 requisições
+  message: 'Muitas tentativas tente mais tarde.',
+});
 
 app.use(cors());
 const connectDB = require('./db');
@@ -13,6 +20,7 @@ app.use(express.json());
 
 connectDB();
 
+app.use('/auth/login', loginLimiter);
 app.use('/auth', authRoutes);
 app.use('/game', gameRoutes);
 
